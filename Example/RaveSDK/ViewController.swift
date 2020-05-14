c4c86f3791c2b5dca2ff42d778c27b478bc8c05e
Olusegun Solaja <segun.solaja@flutterwavego.com>
2020-05-14 10:21:20 -0400

    bug fixes

--- Example/RaveSDK/ViewController.swift
+++ Example/RaveSDK/ViewController.swift
@@ -31,8 +31,8 @@ class ViewController: UIViewController ,RavePayProtocol {
         config.lastName = "TEST" //lastname
         config.meta = [["metaname":"sdk", "metavalue":"ios"]]
         
-        config.publicKey = "FLWPUBK_TEST-edf1c9cf02e1280c418a2a50f32a40c5-X" //Public key
-        config.encryptionKey = "FLWSECK_TESTa2db7a79561a" //Encryption key
+        config.publicKey = "[PUB_KEY]" //Public key
+        config.encryptionKey = "[ENCRYPTION_KEY]" //Encryption key
 
         
         let controller = NewRavePayViewController()
