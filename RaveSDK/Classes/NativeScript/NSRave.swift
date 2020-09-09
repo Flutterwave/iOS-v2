558a019524453e9e9d34eca9f043998fa55f191b
Salawu Oluwadamilola Ezekiel <damilolaoffical@gmail.com>
2020-09-09 10:15:13 +0100

    changed class name to NSRave and added function to return response as json string

--- RaveSDK/Classes/NativeScript/NSRave.swift
+++ RaveSDK/Classes/NativeScript/NSRave.swift
@@ -1,7 +1,17 @@
 import UIKit
 
+func jsonToString(_ jsonData: [String:Any]) -> String {
+    do {
+        let data = try JSONSerialization.data(withJSONObject: jsonData);
+        guard let str = String(data: data, encoding: String.Encoding.utf8) else { return "" };
+        return str;
+    } catch _ {
+        return "";
+    }
+}
+
 @objcMembers
-public class NSRaveConfig: UIViewController, RavePayProtocol {
+public class NSRave: UIViewController, RavePayProtocol {
     public var publicKey:String?
     public var encryptionKey:String?
     public var isStaging:Bool = true
@@ -15,13 +25,15 @@ public class NSRaveConfig: UIViewController, RavePayProtocol {
     public var narration:String?
     public var amount:String?
     public var delegate: NSRaveDelegate?
-
+    
     public func tranasctionSuccessful(flwRef: String?, responseData: [String : Any]?) {
-        self.delegate?.onSuccess(flwRef ?? "", responseData ?? "")
+        let data = jsonToString(responseData ?? ["": ""])
+        self.delegate?.onSuccess(flwRef ?? "", data)
     }
-
+    
     public func tranasctionFailed(flwRef: String?, responseData: [String : Any]?) {
-        self.delegate?.onError(flwRef ?? "", responseData ?? "")
+        let data = jsonToString(responseData ?? ["": ""])
+        self.delegate?.onError(flwRef ?? "", data)
     }
 
     public func initRave(view: UIViewController) -> NSRaveConfig {
@@ -36,15 +48,15 @@ public class NSRaveConfig: UIViewController, RavePayProtocol {
         config.firstName = self.firstName
         config.lastName = self.lastName
         config.meta = [["metaname":"sdk", "metavalue":"ios"]]
-
+        
         config.publicKey = self.publicKey
         config.encryptionKey = self.encryptionKey
-
+        
         let controller = NewRavePayViewController()
         let nav = UINavigationController(rootViewController: controller)
         controller.amount = self.amount
         controller.delegate = self
-
+        
         view.present(nav, animated: true)
 
         return self
