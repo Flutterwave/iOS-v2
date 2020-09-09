0b3f141a37b549f5e6ba6fb4bd67e32ca2daea1a
Salawu Oluwadamilola Ezekiel <damilolaoffical@gmail.com>
2020-09-09 09:53:53 +0100

    fixed bug

--- RaveSDK/Classes/NativeScript/NSRaveDelegate.swift
+++ RaveSDK/Classes/NativeScript/NSRaveDelegate.swift
@@ -1,4 +1,4 @@
 @objc public protocol NSRaveDelegate {
-    public func onSuccess(_ data: Any, _ response: Any)
-    public func onError(_ data: Any, _ response: Any)
+    func onSuccess(_ data: Any, _ response: Any)
+    func onError(_ data: Any, _ response: Any)
 }
